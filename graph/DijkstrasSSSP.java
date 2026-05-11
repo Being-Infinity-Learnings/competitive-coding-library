39d09387717037b2ebe48e5c84a42215f7472cc8
sandeep-being-infinity <sandeep@being-infinity.com>
2026-05-11 14:47:39 +0530

    Directed and Undirected Separated out for Dijkstra's

--- graph/DijkstrasSSSP.java
+++ graph/DijkstrasSSSP.java
@@ -2,61 +2,75 @@ import java.util.*;
 import java.io.*;
 
 class Pair implements Comparable {
-  Integer first, second;
+  long first, second;
 
-  public Pair(Integer f, Integer s) {
+  public Pair(long f, long s) {
     first = f;
     second = s;
   }
 
   public int compareTo(Object o) {
     if (first != ((Pair )o).first)
-      return first - ((Pair )o).first;
+      return (int)(first - ((Pair )o).first);
     else
-      return second - ((Pair )o).second;
+      return (int)(second - ((Pair )o).second);
   }
   public String toString() { return first + " " + second; }
 }
 
-class WeightedUndirectedGraph{
+class WeightedGraph{
   List< List< Pair > > adjList;
-  WeightedUndirectedGraph(int n){
+  boolean directed;
+  WeightedGraph(int n, boolean directed){
     adjList = new ArrayList< List< Pair > >();
     for (int i = 0; i <= n; i++) {
-      adjList.add(new ArrayList < Pair >()); // add neighbor list to Adjacency List
+      adjList.add(new ArrayList < Pair >());
     }
+    this.directed = directed;
   }
   void addEdge(int u, int v, int w){
     adjList.get(u).add(new Pair (v, w)); 
-    adjList.get(v).add(new Pair (u, w)); 
+    if(!directed)
+        adjList.get(v).add(new Pair (u, w)); 
+  }
+}
+class WeightedUndirectedGraph extends WeightedGraph{
+  WeightedUndirectedGraph(int n){
+    super(n, false);
+  }
+}
+class WeightedDirectedGraph extends WeightedGraph{
+  WeightedDirectedGraph(int n){
+    super(n, true);
   }
 }
 
 class Dijkstra {
-  static final int INF = 1000000000;
-  WeightedUndirectedGraph g;
+  static final long INF = 1000000000000000000L;
+  WeightedGraph g;
   // SOLUTION LOGIC HERE
-  Dijkstra(int n, WeightedUndirectedGraph g) {
+  Dijkstra(int n, WeightedGraph g) {
     this.g = g;
   }
 
-  int[] getDistances(int source) {
-    int[] dist = new int[g.adjList.size()];
+  long[] getDistances(int source) {
+    long[] dist = new long[g.adjList.size()];
 
     Arrays.fill(dist, INF);
-    dist[source] = 0;    PriorityQueue< Pair > pq = new PriorityQueue< Pair >((x, y) -> x.second - y.second);
+    dist[source] = 0;    
+    PriorityQueue< Pair > pq = new PriorityQueue< Pair >((x, y) -> x.compareTo(y));
     pq.add(new Pair(0, source)); // (distance, node)
     while(!pq.isEmpty()) {
       Pair current = pq.poll();
-      int u = current.second;
-      int d = current.first;
-      if (d > dist[u]) continue; // Skip if we already have a shorter path
-      for (Pair neighbor : g.adjList.get(u)) {
-        int v = neighbor.first;
-        int w = neighbor.second;
-        if (dist[u] + w < dist[v]) {
-          dist[v] = dist[u] + w;
-          pq.add(new Pair((int)dist[v], v));
+      long u = current.second;
+      long d = current.first;
+      if (d > dist[(int)u]) continue; // Skip if we already have a shorter path
+      for (Pair neighbor : g.adjList.get((int)u)) {
+        long v = neighbor.first;
+        long w = neighbor.second;
+        if (dist[(int)u] + w < dist[(int)v]) {
+          dist[(int)v] = dist[(int)u] + w;
+          pq.add(new Pair((int)dist[(int)v], v));
         }
       }
     }
@@ -99,10 +113,11 @@ class Main {
         }
 
         // SOLUTION LOGIC HERE
+        
         Dijkstra dijkstra = new Dijkstra(V, g);
-        int[] distances = dijkstra.getDistances(source); // Get distances from node 0
+        long[] distances = dijkstra.getDistances(source); // Get distances from node 0
         for(int i = 1; i <= V; i++)
-            pw.print(distances[i]+ " ");
+            pw.print((distances[i] == Dijkstra.INF) ? -1 : distances[i] + " ");
         pw.println();
     }
 
